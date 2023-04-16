4fec5b835bcbc9a85e81bd1a85cdb6d0d4a87fce
Mihir Wankhade <87888969+mihirwankhade10@users.noreply.github.com>
2023-04-16 19:28:10 +0530

    connected to cloud database

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,8 +2,8 @@
 const mongoose = require('mongoose');
 
 // connect from mongodb
- mongoose.connect('mongodb://127.0.0.1:27017/Review-System');
-//mongoose.connect('mongodb+srv://ashishpal:123abc@ers.vd4pr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+// mongoose.connect('mongodb://127.0.0.1:27017/Review-System');
+   mongoose.connect('mongodb+srv://wankhadeabhi3:abhi123456@cluster0.jhzupam.mongodb.net/review-system?retryWrites=true&w=majority');
 
 // aquire connection if it is succesful
 const db = mongoose.connection;
